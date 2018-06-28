ce0f601f99ce714e7b4b2bd56a1017ec157a91a1
Chad <chadrtyler@gmail.com>
2018-06-27 21:07:39 -0400

    Added color variable in main function

--- Fractal - Udemy/Fractal - Udemy/main.cpp
+++ Fractal - Udemy/Fractal - Udemy/main.cpp
@@ -29,16 +29,17 @@ int main(int argc, const char * argv[]) {
         for(int x{0}; x < WIDTH; ++x)
         {
             
-            double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+            double xFractal = (x - WIDTH/2 - 200) * 2.0/HEIGHT;
             double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
             
             int iterations = slick::Mandelbrot::getIterations(xFractal, yFractal);
-            std::uint8_t red = (std::uint8_t)(256 * (double)iterations/slick::Mandelbrot::MAX_ITERATIONS);
+            std::uint8_t color = (std::uint8_t)(256 * (double)iterations/slick::Mandelbrot::MAX_ITERATIONS);
             
+            color = color*color*color;
             
-            bitmap.setPixel(x,y, red, red, red);
-            if(red < min) min = red;
-            if(red > max) max = red;
+            bitmap.setPixel(x,y, 0, color, 0);
+            if(color < min) min = color;
+            if(color > max) max = color;
         }
     }
     
